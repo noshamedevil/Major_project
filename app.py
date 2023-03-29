94fb3b90fa70248ec1beebeea205476c7f2090f8
Nimish Pandey <pandeynimish2001@gmail.com>
2023-03-29 12:47:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -397,14 +397,14 @@ if uploaded_file is not None:
         
 # ==========================================================================================================================================
     st.sidebar.info("After using this app please Click Clear Cache button so that your all data is removed from the folder.")
-    if st.sidebar.button("Clear Cache"):
-        clear_image_cache()
+   # if st.sidebar.button("Clear Cache"):
+    #    clear_image_cache()
 
-else:
-    with open('samples/sample.zip', 'rb') as f:
-        st.sidebar.download_button(
-                label="Download Sample Data and Use It",
-                data=f,
-                file_name='smaple_data.zip',
-                help = "Download some sample data and use it to explore this web app."
-            )
+#else:
+ #   with open('samples/sample.zip', 'rb') as f:
+  #      st.sidebar.download_button(
+   #             label="Download Sample Data and Use It",
+    #            data=f,
+     #           file_name='smaple_data.zip',
+      #          help = "Download some sample data and use it to explore this web app."
+       #     )
