886a525577929eb178c936f6ee52e1f9aba84049
Nimish Pandey <pandeynimish2001@gmail.com>
2023-04-10 19:40:39 +0530

    Updated files

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("rb") as file:
     hashed_passwords = pickle.load(file)
 
-#authenticator=stauth.Authenticate(names, usernames,hashed_passwords,"data_analysis", "abcdef", cookie_expiry_days=30)
+authenticator=stauth.Authenticate(names, usernames,hashed_passwords,"data_analysis", "abcdef", cookie_expiry_days=3)
 name, authentication_status, username = authenticator.login("Login","main")
 
 if authentication_status == False:
